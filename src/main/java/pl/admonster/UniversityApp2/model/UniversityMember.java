c5ff3d928e718e740d4ab6c1e1b8c4c6c30dff75
VicenteJankowski <jxstudiopl@gmail.com>
2022-12-08 23:33:13 +0100

    Add: validation methods for age and e-mail

--- src/main/java/pl/admonster/UniversityApp2/model/UniversityMember.java
+++ src/main/java/pl/admonster/UniversityApp2/model/UniversityMember.java
@@ -7,6 +7,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.security.InvalidParameterException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @MappedSuperclass
 @Setter
 @Getter
@@ -32,4 +36,19 @@ abstract class UniversityMember {
 
     private String email;
 
+    public void setAge(int age) throws IllegalArgumentException{
+        if(age <= 18)
+            throw new IllegalArgumentException("age under 19");
+        this.age = age;
+    }
+
+    public void setEmail(String email) {
+        String regex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+        Pattern emailValidation = Pattern.compile(regex);
+        Matcher matcher = emailValidation.matcher(email);
+
+        if(!matcher.matches())
+            throw new InvalidParameterException("wrong e-mail address");
+        this.email = email;
+    }
 }
