2978de0687fca68ba6b4fe891d57dcc3e6488bbd
VicenteJankowski <jxstudiopl@gmail.com>
2022-12-08 23:07:26 +0100

    Added DELETE endpoints

--- src/main/java/pl/admonster/UniversityApp2/UniversityController.java
+++ src/main/java/pl/admonster/UniversityApp2/UniversityController.java
@@ -3,10 +3,7 @@ package pl.admonster.UniversityApp2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,6 +12,9 @@ public class UniversityController {
 
     @Autowired
     TeacherRepository teacherRepository;
+
+    @Autowired
+    StudentRepository studentRepository;
 
     @GetMapping("/test")
     public String runTest(){
@@ -27,7 +27,7 @@ public class UniversityController {
     }
 
     @PostMapping("/teacher")
-    public ResponseEntity<Teacher> createTeacher(@RequestBody Teacher teacher) {
+    public ResponseEntity<Teacher> createTeacher(@RequestBody final Teacher teacher) {
         List<Teacher> existingTeachers = teacherRepository.findAll();
 
         if(existingTeachers.contains(teacher))
@@ -37,5 +37,26 @@ public class UniversityController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @PostMapping("/student")
+    public ResponseEntity<Student> createStudent(@RequestBody final Student student) {
+        List<Teacher> existingTeachers = teacherRepository.findAll();
 
+        if(existingTeachers.contains(student))
+            return ResponseEntity.unprocessableEntity().build();
+        studentRepository.save(student);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/teacher/{id}")
+    public ResponseEntity<HttpStatus> deleteTeacher(@PathVariable("id") final Long id){
+        teacherRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/student/{id}")
+    public ResponseEntity<HttpStatus> deleteStudent(@PathVariable("id") final Long id){
+        studentRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
